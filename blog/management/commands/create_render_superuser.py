51729b99751f68a5140c961da57fc2e4146ccce0
Jayhind Yadav <yadavjayhind692@gmail.com>
2026-02-07 23:33:53 +0530

    fix superuser render

--- blog/management/commands/create_render_superuser.py
+++ blog/management/commands/create_render_superuser.py
@@ -2,22 +2,23 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 import os
 
-class Command(BaseCommand):
-    help = "Create superuser for Render automatically"
+User = get_user_model()
 
-    def handle(self, *args, **kwargs):
-        User = get_user_model()
+class Command(BaseCommand):
+    help = "Create superuser automatically on Render"
+
+    def handle(self, *args, **options):
 
         username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
         email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
         password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
         if not username or not password:
-            self.stdout.write(self.style.WARNING("Superuser env variables not found"))
+            self.stdout.write(self.style.WARNING("Superuser env vars not found"))
             return
 
         if User.objects.filter(username=username).exists():
-            self.stdout.write(self.style.SUCCESS("Superuser already exists"))
+            self.stdout.write(self.style.WARNING("Superuser already exists"))
             return
 
         User.objects.create_superuser(
